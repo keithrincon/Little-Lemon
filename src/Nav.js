5a12b70a90ffbcf5cfd9838c50c013db004638a5
Keith Rincon <keithrt3008@gmail.com>
2023-04-08 13:38:12 -0700

    styled elements

--- src/Nav.js
+++ src/Nav.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Nav(props) {
   return (
-    <nav>
-      <ul>
-      <img src={props.logo} alt="Logo" />
+   <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <img src={props.logo} alt="Logo" style={{ width: '150px', height: 'auto' }} />
+      <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Menu</a></li>
