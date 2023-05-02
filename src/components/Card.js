1b1d1bc6e16d699fddfb3e30f2ce6aef6213af12
Keith Rincon <keithrt3008@gmail.com>
2023-05-01 17:15:47 -0700

    Week 4 final

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,53 +1,35 @@
-.cards {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    margin-top: 2rem;
-  }
-  
-  .card {
-    background-color: #edefee;
-    max-width: 300px;
-    border-radius: 16px 16px 0 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-  
-  .card img {
-    border-radius: 16px 16px 0 0;
-    width: 300px;
-    max-height: 30%;
-    object-fit: cover;
-  }
-  
-  .card .dish-info {
-    padding: 1.5rem;
-    height: 70%;
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .card .title {
-    font-size: 18px;
-    font-weight: 600;
-    display: flex;
-    justify-content: space-between;
-  }
-  
-  .card .price {
-    color: orange;
-    font-weight: 600;
-  }
-  
-  .card p:not(.price) {
-    font-size: 1rem;
-    line-height: 1.5;
-    color: #333;
-    margin: 2rem 0;
-  }
-  
-  .specials-order-btn {
-    margin-top: auto;
-    cursor: pointer;
-  }
+import React from "react";
+import { MdDeliveryDining } from "react-icons/md";
+import "./Card.css";
+
+const Card = ({ image, title, price, description }) => {
+  return (
+    <div className="card">
+      <img width="300px" src={image} alt="greek salad" />
+      <div className="dish-info">
+        <div className="title">
+          <h3>{title}</h3>
+          <p className="price">${price}</p>
+        </div>
+        <p>{description}</p>
+        <span
+          role="button"
+          aria-label="On Click"
+          className="specials-order-btn"
+        >
+          Order a delivery
+          <MdDeliveryDining
+            size={30}
+            style={{
+              color: "#333",
+              marginLeft: "15px",
+              marginBottom: "-10px",
+            }}
+          />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
